3d255dd62875132ebdc7ff8260b510557f16b8a4
Richard Rosenblat <rosenblatr@gmail.com>
2024-09-29 23:55:39 -0300

    refactor: Add GrayScaleFilter to FilterLibraryV1

--- src/entity/FilterLibraries/LibraryV1.ts
+++ src/entity/FilterLibraries/LibraryV1.ts
@@ -1,6 +1,11 @@
 import { IFilter, IFilterLibrary } from "../../types/IFilterLibrary";
 import { GrayScaleFilter } from "../Filters/grayscale";
 
+/**
+ * Filters library version 1
+ * This library contains the following filters:
+ * - GrayScaleFilter
+ */
 export class FilterLibraryV1 implements IFilterLibrary {
 	[x: string]: IFilter;
 	GrayScaleFilter: GrayScaleFilter = new GrayScaleFilter();
