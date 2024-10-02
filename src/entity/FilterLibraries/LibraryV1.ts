c3f677321c35d2a6b8d2be6beb0a9d8761df591e
Richard Rosenblat <rosenblatr@gmail.com>
2024-10-01 21:32:48 -0300

    refactor: Remove unused import and update FilterLibraryV1 to use object literal

--- src/entity/FilterLibraries/LibraryV1.ts
+++ src/entity/FilterLibraries/LibraryV1.ts
@@ -1,4 +1,4 @@
-import { IFilter, IFilterLibrary } from "../../types/IFilterLibrary";
+import { IFilterLibrary } from "../../types/IFilterLibrary";
 import { GrayScaleFilter } from "../Filters/grayscale";
 
 /**
@@ -6,7 +6,7 @@ import { GrayScaleFilter } from "../Filters/grayscale";
  * This library contains the following filters:
  * - GrayScaleFilter
  */
-export class FilterLibraryV1 implements IFilterLibrary {
-	[x: string]: IFilter;
-	GrayScaleFilter: GrayScaleFilter = new GrayScaleFilter();
+export const FilterLibraryV1: IFilterLibrary = {
+	GrayScaleFilter: new GrayScaleFilter(),
 }
+
